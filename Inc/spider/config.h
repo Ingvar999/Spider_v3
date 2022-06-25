f54231b6cceec1f0e124dcb8084764200016be40
Ihar Shymanski <shymanski.ihar@gmail.com>
2022-06-26 00:34:12 +0300

    [+] Config management

--- Inc/spider/config.h
+++ Inc/spider/config.h
@@ -10,6 +10,17 @@
 
 #include "defines.h"
 
+#define CONFIG_FLASH_ADDR						(0x08000000)
+#define CONFIG_FLASH_MAGIC					(0xFAAC8CE1)
+
+typedef enum {
+	CFG_SUCCESS = 0,
+	CFG_READ_ERROR,
+	CFG_ERASE_ERROR,
+	CFG_WRITE_ERROR,
+	CFG_NOT_INITIALIZED,
+} config_status_t;
+
 typedef struct {
 	int speed;
 	int position_v;
@@ -21,4 +32,6 @@ typedef struct {
 extern config_t global_config;
 
 void init_config(void);
-
+config_status_t restore_config(void);
+config_status_t persist_config(void);
+config_status_t reset_config(void);
--- Src/spider/config.c
+++ Src/spider/config.c
@@ -10,6 +10,12 @@
 #include "config.h"
 #include "debug_utils.h"
 
+typedef struct {
+	uint32_t magic;
+	config_t config;
+	uint32_t checksum;
+} flash_config_t;
+
 config_t global_config;
 static const config_t default_config = {
 	3, // speed
@@ -19,7 +25,110 @@ static const config_t default_config = {
 	true, // gyro control enable
 };
 
-void init_config(void)
+static void set_default_config()
 {
 	memcpy(&global_config, &default_config, sizeof(config_t));
+}
+
+static uint32_t calc_checksum(const uint32_t *data, uint32_t size)
+{
+	uint32_t res = 0;
+	size /= sizeof(uint32_t);
+	
+	while(size)
+	{
+		res += (*data) * (size--);
+		++data;
+	}
+	return res;
+}	
+
+void init_config(void)
+{
+	if (restore_config() != CFG_SUCCESS)
+	{
+		LOG_WARN("Unable to load config, use default");
+		set_default_config();
+	}
+}
+
+config_status_t restore_config(void)
+{
+	config_status_t status = CFG_SUCCESS;
+	const flash_config_t *cfg = (void*)CONFIG_FLASH_ADDR;
+	
+	if ((cfg->magic == CONFIG_FLASH_MAGIC) ||
+			(calc_checksum((uint32_t *)&cfg->config, sizeof(config_t)) == cfg->checksum))
+	{
+		memcpy(&global_config, &cfg->config, sizeof(config_t));
+	}
+	else
+	{
+		status = CFG_NOT_INITIALIZED;
+	}
+	return status;
+}
+
+config_status_t persist_config(void)
+{
+	config_status_t status = CFG_SUCCESS;
+	const flash_config_t *cfg = (void*)CONFIG_FLASH_ADDR;
+	
+	uint32_t checksum = calc_checksum((uint32_t*)&global_config, sizeof(config_t));
+	if (checksum != cfg->checksum)
+	{
+		HAL_FLASH_Unlock();
+		uint32_t sector;
+		FLASH_EraseInitTypeDef erase_typedef;
+	
+		erase_typedef.TypeErase = FLASH_TYPEERASE_SECTORS;
+		erase_typedef.Sector = FLASH_SECTOR_0;
+		erase_typedef.NbSectors = 1;
+		erase_typedef.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+		if (HAL_FLASHEx_Erase(&erase_typedef, &sector) == HAL_OK)
+		{
+			if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, (uint32_t)&cfg->magic, CONFIG_FLASH_MAGIC) == HAL_OK)
+			{
+			  int size = sizeof(config_t) / sizeof(uint32_t);
+				uint32_t dst = (uint32_t)&cfg->config;
+				uint32_t *src = (uint32_t*)&global_config;
+				
+				while (size--)
+				{
+					if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, dst, *src) != HAL_OK)
+					{
+						status = CFG_WRITE_ERROR;
+						break;
+					}
+					dst += sizeof(uint32_t);
+					++src;
+				}
+				if (status == CFG_SUCCESS)
+				{
+					if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, (uint32_t)&cfg->checksum, checksum) != HAL_OK)
+					{
+						status = CFG_WRITE_ERROR;
+					}
+				}
+			}
+			else
+			{
+				status = CFG_WRITE_ERROR;
+			}
+		}
+		else
+		{
+			status = CFG_ERASE_ERROR;
+		}
+		
+		HAL_FLASH_Lock();
+	}
+		
+	return status;
+}
+
+config_status_t reset_config(void)
+{
+	set_default_config();
+	return persist_config();
 }
